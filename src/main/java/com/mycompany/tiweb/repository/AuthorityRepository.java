dca7b41bfbb9a3a7dbe39fcd7829a4c9bcde1d44
Pascal Grimaud <pascalgrimaud@gmail.com>
2019-10-13 18:59:11 +0200

    Initial version of tiweb generated by JHipster-6.3.1

---
+++ src/main/java/com/mycompany/tiweb/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.mycompany.tiweb.repository;
+
+import com.mycompany.tiweb.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
